0c3b1c2d2fda1b89acdf0bad6e8b99b04d10d837
meghan <lindholm.megh@gmail.com>
2018-05-16 03:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Meghan the Herbal WitchSlut"
-var height = "999"
+var height = 999
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
